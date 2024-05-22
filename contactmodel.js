c031937ee0353d97777c2aebc62014c8d6198c16
pkalsi97 <pranjulkalsi@gmail.com>
2024-05-22 22:40:19 +0530

    Contact Model Set-up

---
+++ contactmodel.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./db');
+
+const Contact = sequelize.define('Contact', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  linkedId: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  linkPrecedence: {
+    type: DataTypes.ENUM('secondary', 'primary'),
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  timestamps: true,
+  paranoid: true,
+  tableName: 'Contact'
+});
+
+module.exports = Contact;
