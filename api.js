302e729f614815a6faaa639adeb5cf63dd850076
pkalsi97 <pranjulkalsi@gmail.com>
2024-05-22 23:14:37 +0530

    Api endpoint when called uses Vaildate function

--- api.js
+++ api.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { validatePayload } = require('./identifyService');
 const { Contact } = require('./contactmodel');
 
 const apiRouter = express.Router();
 
-apiRouter.post('/identify', (req, res) => {
+apiRouter.post('/identify', validatePayload, (req, res) => {
   const { email, phoneNumber } = req.body;
   res.json({ email, phoneNumber });
 });
